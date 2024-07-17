e333dd1ab25579ff6feee8138c9421f9a15f25e8
anmol <guptaanmol164@gmail.com>
2024-07-17 19:42:29 +0530

    Initializing Frontend and Backend Application

---
+++ dementia-aid-backend/src/main/java/com/anmol/dementia_aid_backend/DementiaAidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anmol.dementia_aid_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DementiaAidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DementiaAidBackendApplication.class, args);
+	}
+
+}
---
+++ dementia-aid-backend/src/test/java/com/anmol/dementia_aid_backend/DementiaAidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmol.dementia_aid_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DementiaAidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
